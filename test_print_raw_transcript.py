0e2f531911b12b6c057920c4abca15c9924cd60b
David <scaperot@vt.edu>
2020-12-12 11:21:03 -0500

    initial commit.

---
+++ test_print_raw_transcript.py
@@ -0,0 +1,27 @@
+
+import matplotlib.pyplot as plt
+import numpy as np
+import os,time,sys,argparse
+
+import pdb
+import DALI as dali_code
+from DALI import utilities
+
+import dali_helpers
+
+
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description='print a song transcript')
+    parser.add_argument('-s','--song-id', required=False,type=str,default='3698c37beab64ec39196875d69720822',
+            help='DALI song id. default: 3698c37beab64ec39196875d69720822')
+
+    args = parser.parse_args()
+    
+    song_id = args.song_id
+    dali_helpers.print_raw_transcript(song_id)
+
+
+
